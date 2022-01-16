dee963882b04a999fc9c20829e17f2ac650bd0a2
CinthyaSal <cinthya.karla95@gmail.com>
2022-01-16 11:43:53 -0600

    Cambios2

--- sketch.js
+++ sketch.js
@@ -18,6 +18,8 @@ function setup(){
 
     suelo2= new Ground(200,height,1400,45);
     ///agregar cambios
+
+    //cambio 2
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
