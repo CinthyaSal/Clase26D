3edc23a4fcdb335f7cfef89a6784165fd413f4ee
CinthyaSal <cinthya.karla95@gmail.com>
2022-01-16 11:39:54 -0600

    Cambios

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var suelo2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +16,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    suelo2= new Ground(200,height,1400,45);
+    ///agregar cambios
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
